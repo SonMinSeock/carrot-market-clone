13a50ded459ab69f8d061f174eae8f44d83e3020
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-18 14:36:02 +0900

    Test Drive Part Two 4.2

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,29 @@ const Home: NextPage = () => {
           Checkout
         </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-3xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14 flex justify-between">
+          <span className="text-white text-2xl">Profile</span>
+          <span className="text-white text-2xl">장바구니</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <img className="h-24 w-24  rounded-full" src="/avatar.jpeg" />
+            <div className="flex flex-col items-center">
+              <span className="text-sm text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative  flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Son MinSeock</span>
+            <span className="text-sm text-gray-500">한국</span>
+          </div>
+        </div>
+      </div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
       <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
