223e8b1fd9854ba51d5de297c448665f71555447
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-20 21:00:19 +0900

    Dark Mode 4.11

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,26 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-slate-400 py-20 px-10 grid lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center gap-10 min-h-screen">
-      <div className="bg-white flex flex-col justify-between p-6 rounded-3xl shadow-xl">
-        <span className="font-semibold text-3xl">Select Item</span>
+    <div className="bg-slate-400 dark py-20 px-10 grid lg:grid-cols-2 xl:grid-cols-3 xl:place-content-center gap-10 min-h-screen">
+      <div className="bg-white dark:bg-black flex flex-col justify-between p-6 rounded-3xl shadow-xl">
+        <span className="font-semibold dark:text-white text-3xl">
+          Select Item
+        </span>
         <div>
           <div className="flex justify-between my-2">
-            <span className="text-gray-500">Grey Chair</span>
-            <span className="font-semibold">$19</span>
+            <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
+            <span className="font-semibold dark:text-white">$19</span>
           </div>
           <div className="flex justify-between my-2">
-            <span className="text-gray-500">Grey Chair</span>
-            <span className="font-semibold">$19</span>
+            <span className="text-gray-500 dark:text-gray-100">Grey Chair</span>
+            <span className="font-semibold dark:text-white">$19</span>
           </div>
         </div>
         <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
           <span className="font-semibold">$10</span>
         </div>
-        <button className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-3/4 mx-auto hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
+        <button className="mt-5 bg-blue-500 text-white dark:bg-black dark:border-white dark:border p-3 text-center rounded-xl w-3/4 mx-auto dark:hover:bg-white hover:bg-teal-500 hover:text-black active:bg-yellow-500 focus:bg-red-500">
           Checkout
         </button>
       </div>
