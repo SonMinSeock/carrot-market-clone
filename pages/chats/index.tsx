1202b8d2f2ea627550624300bf1d47d65f150b00
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-24 19:48:11 +0900

    Layout part Two 3.17

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,3 +1,4 @@
+import Layout from "@/components/layout";
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
 
@@ -6,23 +7,25 @@ const Chats: NextPage = () => {
 
   const navigateChatDetail = (id: number) => router.push(`/chats/${id}`);
   return (
-    <div className="py-10 divide-y-[1px]">
-      {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-        <div
-          key={i}
-          className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-          onClick={() => navigateChatDetail(i)}
-        >
-          <div className="w-12 h-12 rounded-full bg-slate-300" />
-          <div>
-            <p className="text-gray-700">Steve Jebs</p>
-            <p className="text-sm  text-gray-500">
-              See you tomorrow in the corner at 2pm!
-            </p>
+    <Layout title="채팅" hasTabBar>
+      <div className="py-10 divide-y-[1px]">
+        {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <div
+            key={i}
+            className="flex px-4 cursor-pointer py-3 items-center space-x-3"
+            onClick={() => navigateChatDetail(i)}
+          >
+            <div className="w-12 h-12 rounded-full bg-slate-300" />
+            <div>
+              <p className="text-gray-700">Steve Jebs</p>
+              <p className="text-sm  text-gray-500">
+                See you tomorrow in the corner at 2pm!
+              </p>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </Layout>
   );
 };
 
