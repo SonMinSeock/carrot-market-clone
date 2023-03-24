1ee95869d4b937985e283806510c12a66fbbf5eb
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-24 20:50:08 +0900

    Conclusions 5.18

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -1,28 +1,23 @@
-import Layout from "@/components/layout";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import Link from "next/link";
+import Layout from "@/components/layout";
 
 const Chats: NextPage = () => {
-  const router = useRouter();
-
-  const navigateChatDetail = (id: number) => router.push(`/chats/${id}`);
   return (
     <Layout title="채팅" hasTabBar>
       <div className="py-10 divide-y-[1px]">
         {[1, 1, 1, 1, 1, 1, 1].map((_, i) => (
-          <div
-            key={i}
-            className="flex px-4 cursor-pointer py-3 items-center space-x-3"
-            onClick={() => navigateChatDetail(i)}
-          >
-            <div className="w-12 h-12 rounded-full bg-slate-300" />
-            <div>
-              <p className="text-gray-700">Steve Jebs</p>
-              <p className="text-sm  text-gray-500">
-                See you tomorrow in the corner at 2pm!
-              </p>
-            </div>
-          </div>
+          <Link href={`/chats/${i}`} key={i} legacyBehavior>
+            <a className="flex px-4 cursor-pointer py-3 items-center space-x-3">
+              <div className="w-12 h-12 rounded-full bg-slate-300" />
+              <div>
+                <p className="text-gray-700">Steve Jebs</p>
+                <p className="text-sm  text-gray-500">
+                  See you tomorrow in the corner at 2pm!
+                </p>
+              </div>
+            </a>
+          </Link>
         ))}
       </div>
     </Layout>
--- pages/live/index.tsx
+++ pages/live/index.tsx
@@ -1,32 +1,23 @@
-import Layout from "@/components/layout";
 import type { NextPage } from "next";
-import { useRouter } from "next/router";
+import Link from "next/link";
+import FloatingButton from "../../components/floating-button";
+import Layout from "@/components/layout";
 
 const Live: NextPage = () => {
-  const router = useRouter();
-
-  const navigateCreateLive = () => router.push(`/streams/create`);
-
-  const navigateStream = (id: number) => router.push(`/live/${id}`);
-
   return (
     <Layout title="라이브" hasTabBar>
       <div className="py-10  divide-y-2 space-y-4">
         {[1, 2, 3, 4, 5].map((_, i) => (
-          <div className="pt-4 px-4" key={i}>
-            <div className="w-full bg-slate-300 aspect-video rounded-md shadow-sm" />
-            <h3
-              onClick={() => navigateStream(i)}
-              className="text-gray-700 text-2xl mt-2 cursor-pointer inline-block font-bold"
-            >
-              Galaxy S50
-            </h3>
-          </div>
+          <Link key={i} href={`/live/${i}`} legacyBehavior>
+            <a className="pt-4 block  px-4">
+              <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+              <h1 className="text-2xl mt-2 font-bold text-gray-900">
+                Galaxy S50
+              </h1>
+            </a>
+          </Link>
         ))}
-        <button
-          onClick={navigateCreateLive}
-          className="fixed hover:bg-orange-500  transition-colors cursor-pointer bottom-24 right-5 shadow-xl bg-orange-400 rounded-full p-4 text-white border-transparent "
-        >
+        <FloatingButton href="/live/create">
           <svg
             className="w-6 h-6"
             fill="none"
@@ -43,7 +34,7 @@ const Live: NextPage = () => {
               d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"
             />
           </svg>
-        </button>
+        </FloatingButton>
       </div>
     </Layout>
   );
