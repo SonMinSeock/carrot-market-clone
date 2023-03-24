e9dadaf2188c9934257dc35cb419443ba9ab670a
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-24 19:07:57 +0900

    Layout part One 5.16

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -1,8 +1,5 @@
+import { cls } from "@/libs/utils";
 import { useState } from "react";
-
-function cls(...classname: string[]) {
-  return classname.join(" ");
-}
 
 export default function Enter() {
   const [method, setMethod] = useState<"email" | "phone">("email");
