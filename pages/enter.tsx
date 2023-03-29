b37a45cbb9d9579b2f55f84247357ec00e076991
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-29 21:15:23 +0900

    Clean Code part Two 8.3

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -12,7 +12,6 @@ interface EnterForm {
 
 export default function Enter() {
   const [enter, { loading, data, error }] = useMutation("/api/users/enter");
-  const [submitting, setSubmitting] = useState(false);
   const { register, watch, handleSubmit, reset } = useForm<EnterForm>();
   const [method, setMethod] = useState<"email" | "phone">("email");
 
@@ -27,8 +26,11 @@ export default function Enter() {
   };
 
   const onValid = (data: EnterForm) => {
+    if (loading) return;
     enter(data);
   };
+
+  console.log(loading, data, error);
 
   return (
     <div className="mt-16 px-4">
@@ -84,9 +86,11 @@ export default function Enter() {
               required
             />
           ) : null}
-          {method === "email" ? <Button text={"Get login link"} /> : null}
+          {method === "email" ? (
+            <Button text={loading ? "Loading" : "Get login link"} />
+          ) : null}
           {method === "phone" ? (
-            <Button text={submitting ? "Loadding" : "Get one-time password"} />
+            <Button text={loading ? "Loadding" : "Get one-time password"} />
           ) : null}
         </form>
         <div className="mt-8">
