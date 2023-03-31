ac301a43457345f664d4dc4c5f52cd6fea26f9df
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-31 17:16:47 +0900

    Accounts Logic 9.1

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -4,7 +4,22 @@ import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.body);
+  const { phone, email } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+
+  const user = await client.user.upsert({
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+    where: {
+      ...payload,
+    },
+  });
+
+  console.log("user : ", user);
+
   return res.status(200).end();
 }
 
