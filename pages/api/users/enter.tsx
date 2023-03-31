cad29edd5e720951e69932a1013404c16b66d7ae
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-03-31 18:27:57 +0900

    Token Logic 9.2

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -7,18 +7,24 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { phone, email } = req.body;
   const payload = phone ? { phone: +phone } : { email };
 
-  const user = await client.user.upsert({
-    create: {
-      name: "Anonymous",
-      ...payload,
-    },
-    update: {},
-    where: {
-      ...payload,
+  const token = await client.token.create({
+    data: {
+      payload: "1234",
+      user: {
+        connectOrCreate: {
+          where: {
+            ...payload,
+          },
+          create: {
+            name: "Anonymous",
+            ...payload,
+          },
+        },
+      },
     },
   });
 
-  console.log("user : ", user);
+  console.log(token);
 
   return res.status(200).end();
 }
