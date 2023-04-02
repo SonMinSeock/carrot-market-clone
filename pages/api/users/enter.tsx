3a586e0f0f3031928ce764cf3404c9e1593a35f6
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-04-02 17:35:14 +0900

    Sending Email 9.5

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,8 +1,10 @@
 import twilio from "twilio";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
-
 import { NextApiRequest, NextApiResponse } from "next";
+import mail from "@sendgrid/mail";
+
+mail.setApiKey(process.env.SENDGRID_KEY!);
 
 const twilioClient = twilio(
   "AC9b7ce2b2a46f7e7a7279a55fbdb95c55",
@@ -44,6 +46,16 @@ async function handler(
       messagingServiceSid: process.env.TWILIO_MSID,
       to: process.env.MY_PHONE!,
       body: `Your login token is ${payload}.`,
+    });
+  } else if (email) {
+    const email = await mail.send({
+      from: {
+        email: "alstjr2538@naver.com",
+      },
+      to: "alstjr2538@naver.com",
+      subject: "Your Carrot Market Verification Email.",
+      text: `Your Token is ${payload}`,
+      html: `<strong>Your Token is ${payload}</strong>`,
     });
   }
 
