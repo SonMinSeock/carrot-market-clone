b10373d4f1387157a57c81b9e9883cb685e43370
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-04-06 15:50:38 +0900

    Unbound Mutation 11.9

--- pages/products/[id].tsx
+++ pages/products/[id].tsx
@@ -2,11 +2,12 @@ import Layout from "@/components/layout";
 import type { NextPage } from "next";
 import Button from "../../components/button";
 import { useRouter } from "next/router";
-import useSWR from "swr";
+import useSWR, { useSWRConfig } from "swr";
 import Link from "next/link";
 import { Product, User } from "@prisma/client";
 import useMutation from "@/libs/client/useMutation";
 import { cls } from "@/libs/client/utils";
+import useUser from "@/libs/client/useUser";
 
 interface ProductWithUser extends Product {
   user: User;
@@ -21,7 +22,9 @@ interface ItemDetailResponse {
 
 const ItemDetail: NextPage = () => {
   const router = useRouter();
-  const { data, mutate } = useSWR<ItemDetailResponse>(
+  const { user, isLoading } = useUser();
+  const { mutate } = useSWRConfig();
+  const { data, mutate: boundMutate } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
   const [toggleFav] = useMutation(`/api/products/${router.query.id}/fav`);
@@ -30,8 +33,9 @@ const ItemDetail: NextPage = () => {
 
   const onFavoriteClick = () => {
     if (!data) return;
-    mutate({ ...data, isLiked: !data.isLiked }, false);
-    toggleFav({});
+    boundMutate((prev) => prev && { ...prev, isLiked: !prev.isLiked }, false);
+    //mutate("/api/users/me", (prev: any) => ({ ok: !prev.ok }), false);
+    //toggleFav({});
   };
 
   return (
